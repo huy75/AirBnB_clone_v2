d6858a37fcf8d8adcc7da00a56c68912ab07c31b
huy75 <huy.nguyen66@gmail.com>
2021-04-01 10:13:12 +0000

    add comments

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,8 +3,12 @@
 Test for console
 Usage:
     To be used with the unittest module:
-    "python3 -m unittest discover tests" command or
+    "python3 -m unittest discover -v tests" command or
     "python3 -m unittest tests/test_console.py"
+    "HBNB_ENV=test HBNB_MYSQL_USER=hbnb_test
+     HBNB_MYSQL_PWD=hbnb_test_pwd HBNB_MYSQL_HOST=localhost
+     HBNB_MYSQL_DB=hbnb_test_db HBNB_TYPE_STORAGE=db
+     python3 -m unittest discover tests"
 """
 import unittest
 from unittest.mock import create_autospec, patch
