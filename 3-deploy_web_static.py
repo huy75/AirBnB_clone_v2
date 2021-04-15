d4b387a87e5cd02e19afbaab505880282066bae9
huy75 <huy.nguyen66@gmail.com>
2021-04-15 14:54:52 +0000

    feat task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-from os import path
-from fabric.api import env, put, run
+"""
+Python fabric that manages archives for web servers
+"""
+import os
+from fabric.api import *
+from datetime import datetime as dt
 
-env.hosts = ["35.227.45.0", "35.237.153.115"]
+env.hosts = ['35.227.45.0', '35.237.153.115']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
+
+now = dt.now()
+
+
+def do_pack():
+    """Packs web_static files into .tgz file"""
+
+    file_name = 'versions/web_static_{}.tgz'\
+                .format(now.strftime("%Y%m%d%H%M%S"))
+
+    if not os.path.isdir("versions"):
+        if local("mkdir -p versions").failed:
+            return None
+
+    command = local("tar -cvzf {} web_static".format(file_name))
+    if command.succeeded:
+        return file_name
+    return None
 
 
 def do_deploy(archive_path):
@@ -16,12 +37,10 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if not path.exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
-
-    base_name = path.basename(archive_path)
+    base_name = os.path.basename(archive_path)
     name = base_name.split(".")[0]
-
     try:
         """ Upload the archive to the /tmp/ directory of the web server """
         put(archive_path, "/tmp")
@@ -45,9 +64,20 @@ def do_deploy(archive_path):
         Create a new the symbolic link on the web server,
         linked to the new version of the code
         """
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
             .format(name))
         print("New version deployed!")
         return True
     except:
         return False
+
+
+def deploy():
+    """
+    Packs and deploys a webstatic site from an archive
+    Returns: Value of do_deploy, False if no archive created
+    """
+    archive = do_pack()
+    if archive is None:
+        return False
+    return(do_deploy(archive))
