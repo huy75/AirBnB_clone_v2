233dd746f0d77bf6ccd900811d5d4676d6c19427
huy75 <huy.nguyen66@gmail.com>
2021-04-15 13:51:30 +0000

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 from os import path
-from fabric.api import env, put, run
+from fabric.api import env, put, run, sudo
 
 env.hosts = ["35.227.45.0", "35.237.153.115"]
 env.user = 'ubuntu'
@@ -26,13 +26,19 @@ def do_deploy(archive_path):
         """ Upload the archive to the /tmp/ directory of the web server """
         put(archive_path, "/tmp")
 
-        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
         """ Uncompress the archive on the web server """
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(base_name, name))
 
         """ Delete the archive from the web server """
-        run("rm -rf /tmp/{}".format(base_name))
+        run("rm /tmp/{}".format(base_name))
+
+        cmd = "mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/"
+        sudo(cmd.format(name, name))
+
+        run("rm -rf /data/web_static/releases/{}/web_static/")
 
         """ Delete the symbolic link from the web server """
         run("rm -rf /data/web_static/current")
@@ -40,9 +46,9 @@ def do_deploy(archive_path):
         Create a new the symbolic link on the web server,
         linked to the new version of the code
         """
-        run("ln -fs /data/web_static/releases/{} /data/web_static/current"
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
             .format(name))
-        """ run("service nginx restart") """
+        run("service nginx reload")
+        return True
     except:
         return False
-    return True
