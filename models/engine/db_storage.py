1f8560aae27ae5663798a5afbb1c80a66d03d940
huy75 <huy.nguyen66@gmail.com>
2021-04-29 16:04:05 +0000

    feat task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -101,3 +101,8 @@ class DBStorage:
         Session = scoped_session(sessionmaker(bind=self.__engine,
                                               expire_on_commit=False))
         self.__session = Session()
+
+    def close(self):
+        """call remove() method on the private session attribute
+        (self.__session)"""
+        self.__session.close()
