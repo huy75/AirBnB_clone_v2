388edcdaf48df671547b35a89f1487694d5cbba7
huy75 <huy.nguyen66@gmail.com>
2021-04-29 14:59:42 +0000

    fix task 5 with import render_template and feat task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,5 +44,11 @@ def number_template(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
